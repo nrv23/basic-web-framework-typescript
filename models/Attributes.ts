7804d0130b537925c8c465c028fe52ff2d97f380
nrv23 <navemen23@hotmail.com>
2023-08-05 18:43:42 -0600

    solucion a la funcion get de la clase attributes que al llamar la referencia desde la clase user da error de undefined

--- models/Attributes.ts
+++ models/Attributes.ts
@@ -5,7 +5,7 @@ export class Attributes<T extends object>  {
 
     }
 
-    get<K extends keyof T>(key: K): T[K] { // indica que el parametro es una propiedad de la interfaz IUserProps, si se pasa como parametro
+    get = <K extends keyof T>(key: K): T[K] => { // indica que el parametro es una propiedad de la interfaz IUserProps, si se pasa como parametro
         // un nombre de propiedad que no exuiste en a interfaz entonces da error,
         return this.data[key];
     }
